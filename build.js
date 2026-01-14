53345fbc28774de44002b8f912677de6f8a6d43e
Robin Berjon <robin.berjon@nytimes.com>
2026-01-14 17:49:59 +0100

    AT tile working

--- build.js
+++ build.js
@@ -7,6 +7,7 @@ const sources = [
   { from: 'public/demos/experiment/experiment.js', to: 'public/demos/experiment/experiment.min.js' },
   { from: 'public/demos/xdc/xdc.js', to: 'public/demos/xdc/xdc.min.js' },
   { from: 'public/demos/car/car.js', to: 'public/demos/car/car.min.js' },
+  { from: 'public/demos/at/at.js', to: 'public/demos/at/at.min.js' },
   ].map(({ from, to }) => ({
     entryPoints: [from],
     bundle: true,
---
+++ public/demos/at/at.js
@@ -0,0 +1,21 @@
+
+import { TileMothership } from '@dasl/tiles/loader';
+import { ATTileLoader } from '@dasl/tiles/loader/at';
+
+// AT Tiles!
+const at = new ATTileLoader();
+
+const tl = new TileMothership({ loadDomain: 'load.webtiles.bast' });
+tl.init();
+tl.addLoader(at);
+
+const parent = document.createElement('div');
+parent.style.display = 'inline-block';
+parent.style.verticalAlign = 'top';
+parent.style.padding = '50px';
+parent.style.width = '570px';
+document.body.append(parent);
+
+const tile = await tl.loadTile(`at://did:plc:izttpdp3l6vss5crelt5kcux/ing.dasl.masl/3mceykgxbkk2r`);
+console.warn(`Tile`, tile);
+parent.append(await tile.renderCard());
