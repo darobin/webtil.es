248be4cbcb33158a386b5c837e79123ccb8a47a0
Robin Berjon <robin.berjon@nytimes.com>
2026-01-29 18:05:16 +0100

    also header

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 app.set('trust proxy', 'loopback'); // need this
 app.use((req, res, next) => {
   console.warn(`Hostname: ${req.hostname}`);
+  res.header('X-Robin', req.hostname);
   next();
 });
 app.use(createTileLoadingRouter('webtil.es'));
