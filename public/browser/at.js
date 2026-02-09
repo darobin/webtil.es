95a3374271a2a5df35e4da2c39860759fffd504d
Robin Berjon <robin.berjon@nytimes.com>
2026-02-09 14:04:22 +0100

    tutorial

--- public/browser/at.js
+++ public/browser/at.js
@@ -14,21 +14,24 @@ import './el/dyn-img.js';
 //  x need to resolve handles
 //  x get profile data
 //  - loading indicator
+//  - empty page
 //  x load not just the tile, but also details about the user (Tile API?)
 //  x render in a way that shows name, avatar, etc. so it's nice, like a post
-//    - also show tile title and icon when content is rendered
+//    x also show tile title and icon when content is rendered
 //  - handle errors (not found or loading the wrong kind of data) gracefully
 //  x loadDomain should depend on whether we're loaded locally or not
 //  - include a bit of text about where to find more info
 //  - include a view of the data as well, a bit like PDSLS
 //  - minimal wrapper with link back to root, like on DASL specs
-//  - style to make cards and such look nice
+//  x style to make cards and such look nice
 //  - examples:
 //    - of the bad blocked stuff
-//    - with screenshots!
+//    - blog
+//    x with screenshots!
 //  - write spec (AT and CAR)
+//  x tutorial
 //  - announce and link to implementation + browser
-//  - update @dasl/tiles with latest changes
+//  x update @dasl/tiles with latest changes
 
 
 window.addEventListener('hashchange', navigateFromHash);
